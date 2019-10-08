b5f57e17f50b2ba311deabce30d0a05692a5ff58
xpakx <55586470+xpakx@users.noreply.github.com>
2019-10-08 15:06:57 +0200

    feat: make CSV parser pass test for empty line

---
+++ src/main/java/io/github/xpakx/psephopl/utils/CSVUtils.java
@@ -0,0 +1,16 @@
+package io.github.xpakx.psephopl.utils;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CSVUtils 
+{
+ 
+
+  public static List<String> parseLine(String line)
+  {
+    return new ArrayList<String>();
+  }
+  
+ 
+}
