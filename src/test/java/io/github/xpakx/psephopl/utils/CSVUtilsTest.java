81be358fc3fc68cfc3b8154305a27eeec1469e57
xpakx <55586470+xpakx@users.noreply.github.com>
2019-10-08 15:04:08 +0200

    test: test CSV parser for empty line

---
+++ src/test/java/io/github/xpakx/psephopl/utils/CSVUtilsTest.java
@@ -0,0 +1,41 @@
+package io.github.xpakx.psephopl.utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import org.mockito.Mock;
+import org.mockito.InjectMocks;
+import static org.mockito.BDDMockito.*;
+import static org.junit.Assert.*;
+import org.mockito.MockitoAnnotations;
+import org.hamcrest.Matchers;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.hamcrest.Matchers.*;
+import org.mockito.ArgumentCaptor;
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CSVUtilsTest 
+{
+ 
+
+  @Test
+  public void shouldReturnEmptyListIfLineEmpty() throws Exception 
+  {
+    //given
+    String line = "";
+    
+    //when
+    List<String> result = CSVUtils.parseLine(line);
+    
+    //then
+    assertNotNull(result);
+    assertThat(result.size(), is(0));
+  }
+  
+ 
+}
