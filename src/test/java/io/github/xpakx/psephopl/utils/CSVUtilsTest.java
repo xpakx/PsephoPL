9faf1abf1566e63962a1ce2beed66a2e4b6ace6b
xpakx <55586470+xpakx@users.noreply.github.com>
2019-10-08 15:14:29 +0200

    test: test if CSV parser split line by commas

--- src/test/java/io/github/xpakx/psephopl/utils/CSVUtilsTest.java
+++ src/test/java/io/github/xpakx/psephopl/utils/CSVUtilsTest.java
@@ -37,5 +37,38 @@ public class CSVUtilsTest
     assertThat(result.size(), is(0));
   }
   
+  @Test
+  public void shouldReturnEmptyListIfNullLine() throws Exception 
+  {
+    //given
+    String line = null;
+    
+    //when
+    List<String> result = CSVUtils.parseLine(line);
+    
+    //then
+    assertNotNull(result);
+    assertThat(result.size(), is(0));
+  }
+  
+  //RFC 4180 2.4
+  @Test
+  public void shouldSplitLineByCommas()
+  {
+    //given
+    String line = "one,two,three";
+    
+    //when
+    List<String> result = CSVUtils.parseLine(line);
+    
+    //then
+    assertNotNull(result);
+    assertThat(result.size(), is(3));
+    assertThat(result.get(0), is("one"));
+    assertThat(result.get(1), is("one"));
+    assertThat(result.get(2), is("three"));
+  }
+  
+  
  
 }
