15f9832e7611107fb40d4ed72bcd751fe83fcad5
xpakx <55586470+xpakx@users.noreply.github.com>
2019-10-09 07:32:01 +0200

    feat: add fields and getters for Gmina data object

--- src/main/java/io/github/xpakx/psephopl/data/Gmina.java
+++ src/main/java/io/github/xpakx/psephopl/data/Gmina.java
@@ -1,7 +1,37 @@
 package io.github.xpakx.psephopl.data;
 
+import java.util.Set;
+
 public class Gmina
 {
-  private static 
+  private final String teryt;
+  private final String degruba;
+  private final Integer votes;
+  private final Set<Integer> votesForParties;
+  
+  public Gmina(String teryt, String degruba, Integer votes, Set<Integer> votesForParties)
+  {
+    this.teryt = teryt;
+    this.degruba = degruba;
+    this.votes = votes;
+    this.votesForParties = votesForParties;
+  }
+  
+  public String getTERYT()
+  {
+    return teryt;
+  }
+  
+  public String getDEGRUBA()
+  {
+    return degruba;
+  }
+  
+  public Integer getVotes()
+  {
+    return this.votes;
+  }
+  
+  
 
 }
