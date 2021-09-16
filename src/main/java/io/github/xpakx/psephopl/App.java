6358ed78479795b5b6b22d825c1634eddb9fd1d1
xpakx <55586470+xpakx@users.noreply.github.com>
2021-09-16 13:54:54 +0200

    feat: calculate correlation between DGURBA and votes for parties (that's confounded by population size ofc)

--- src/main/java/io/github/xpakx/psephopl/App.java
+++ src/main/java/io/github/xpakx/psephopl/App.java
@@ -1,5 +1,6 @@
 package io.github.xpakx.psephopl;
 
+import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.stat.Correlation;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -47,6 +48,15 @@ public class App
                 .drop("NSI")
                 .drop("LAU_CODE");
 
+        elections2015ByGminasDataSet = elections2015ByGminasDataSet
+                .withColumn("PiS", col("1 - Komitet Wyborczy Prawo i Sprawiedliwość").cast("integer"))
+                .withColumn("PO", col("2 - Komitet Wyborczy Platforma Obywatelska RP").cast("integer"))
+                .withColumn("Razem", col("3 - Komitet Wyborczy Partia Razem").cast("integer"))
+                .withColumn("Korwin", col("4 - Komitet Wyborczy KORWiN").cast("integer"))
+                .withColumn("PSL", col("5 - Komitet Wyborczy Polskie Stronnictwo Ludowe").cast("integer"))
+                .withColumn("SLD", col("6 - Koalicyjny Komitet Wyborczy Zjednoczona Lewica SLD+TR+PPS+UP+Zieloni").cast("integer"))
+                .withColumn("Kukiz", col("7 - Komitet Wyborczy Wyborców „Kukiz'15”").cast("integer"))
+                .na().fill(0, new String[]{"PiS", "PO", "Razem", "Korwin", "PSL", "SLD", "Kukiz"});
 
         System.out.println(degurbaDataSet.count());
         Dataset<Row> gminasWithDegurba = elections2015ByGminasDataSet
@@ -55,7 +65,22 @@ public class App
                         elections2015ByGminasDataSet.col("TERYT").equalTo(degurbaDataSet.col("TERC")),
                         "inner"
                 );
+        gminasWithDegurba = gminasWithDegurba.withColumn("DGURBA_INT", col("DGURBA").cast("integer"));
         gminasWithDegurba.show(5);
         System.out.println(gminasWithDegurba.count());
+
+        Dataset<Row> df = gminasWithDegurba
+                .na().fill(0, new String[]{"DGURBA_INT"})
+                .select(col("DGURBA_INT"), col("PiS"), col("PO"), col("Razem"),
+                        col("Korwin"), col("PSL"), col("SLD"), col("Kukiz"));
+
+        VectorAssembler degurbaVectorAssembler = new VectorAssembler();
+        degurbaVectorAssembler.setInputCols(new String[]{"DGURBA_INT", "PiS", "PO", "Razem", "Korwin", "PSL", "SLD", "Kukiz"});
+        degurbaVectorAssembler.setOutputCol("features");
+        Dataset<Row> newDataSet = degurbaVectorAssembler.transform(df);
+
+        Row corr = Correlation.corr(newDataSet, "features", "pearson").head();
+        System.out.println(corr.get(0));
+
     }
 }
